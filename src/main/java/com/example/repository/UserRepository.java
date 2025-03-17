6d37aa010344b53e7d6d58b2cd9cb6f1b832e1b8
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-17 05:35:04 +0000

    service

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,10 +1,21 @@
 package com.example.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.model.User;
 
 
 public interface UserRepository extends MongoRepository<User,String>{
-    
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    @SuppressWarnings("null")
+    Optional<User> findById(String id);
 }
